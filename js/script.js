38ea20d22397d948a34ea5b3f644b72eecdad171
Amelie Chan <amelie.chan@gmail.com>
2017-02-25 10:09:36 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  getQuote();
+
+var randomQuote;
+var author;
+function getQuote(){
+ var url = "http://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en";
+ $.getJSON(url, function(data){
+   if (data.quoteAuthor === '') {
+   data.quoteAuthor = 'Unknown';
+ }
+   $("#quote").html('"'+data.quoteText+'"');
+   $("#author").html("-"+data.quoteAuthor);
+ });
+};
+
+ $("#tweet").on("click", function(){
+window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+ });
+
+ $("#newQuote").on("click", function(){
+   getQuote();
+ });
+});
