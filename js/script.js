f74bee2d7330c83cc1c2f02011278bdd0abd4902
Amélie <amelie.chan@gmail.com>
2017-02-26 19:27:44 +0000

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
   getQuote();
 
-var randomQuote;
-var author;
 function getQuote(){
- var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+ var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
  $.getJSON(url, function(data){
    if (data.quoteAuthor === '') {
    data.quoteAuthor = 'Unknown';
@@ -14,11 +12,12 @@ function getQuote(){
  });
 };
 
- $("#tweet").on("click", function(){
-window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
- });
-
  $("#newQuote").on("click", function(){
    getQuote();
  });
+  
+ $("#tweet").on("click", function(){
+window.open("https://twitter.com/intent/tweet?text=");
+ });
+  
 });
