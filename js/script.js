234bfe76f4a4f3a76daa720b3acddb8ca339c376
Amélie <amelie.chan@gmail.com>
2017-03-01 20:49:04 +0000

    Update url to add cors-anywhere

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   getQuote();
 
 function getQuote(){
- var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+ var url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en";
  $.getJSON(url, function(data){
    if (data.quoteAuthor === '') {
    data.quoteAuthor = 'Unknown';
